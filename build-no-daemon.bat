10eede6dd8b436e5130a0e70c2c1d68eb718ed16
pjj644 <pjj644@users.noreply.github.com>
2026-05-23 18:11:04 +0800

    update: auto commit

--- build-no-daemon.bat
+++ build-no-daemon.bat
@@ -4,5 +4,5 @@ set JAVA_HOME=D:\deveco\DevEco Studio\jbr
 set DEVECO_SDK_HOME=D:\deveco\DevEco Studio\sdk
 set PATH=%JAVA_HOME%\bin;%PATH%
 cd /d "%~dp0"
-"D:\deveco\DevEco Studio\tools\hvigor\bin\hvigorw.bat" assembleHap %*
+"D:\deveco\DevEco Studio\tools\hvigor\bin\hvigorw.bat" --no-daemon assembleHap %*
 endlocal
